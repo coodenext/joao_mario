3ace23799b9de963ff89bcaba70350bb31182ae4
infotech banda larga <coodenext@gmail.com>
2024-08-18 15:39:30 -0300

    alteração

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('voteBtn').addEventListener('click', function() {
-    // Redirecionar para a página de votação (altere o link conforme necessário)
-    window.location.href = 'https://www.example.com/votacao';
-});
-
-document.getElementById('proposalsBtn').addEventListener('click', function() {
-    // Exibir uma mensagem com propostas (ou redirecionar para uma página de propostas)
-    alert('Aqui estão suas propostas!');
-});
